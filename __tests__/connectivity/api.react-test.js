1c8e7c0ec9ff5c790203ea9398b0a8367c26111f
a6_chris <chris@a6software.co.uk>
2016-10-13 10:26:58 +0100

    test suite refactor

--- __tests__/connectivity/api.react-test.js
+++ __tests__/connectivity/api.react-test.js
@@ -13,10 +13,7 @@ describe('Connectivity', () => {
     it('has a happy path', async () => {
 
       const asyncFetch = require('../../src/connectivity/fetch-json-async-await.js').default;
-      asyncFetch.fetchAsJson = jest.fn(() => { return {
-        ok: true,
-        json: () => 'worked'
-      }});
+      asyncFetch.fetchAsJson = jest.fn(() => 'worked' );
 
       let result = await api.login('bob', 'testpass');
       expect(result).toEqual('worked');
@@ -31,33 +28,6 @@ describe('Connectivity', () => {
       expect(requestConfig.method).toEqual('POST');
     });
 
-
-    it('handles the unhappy path', async () => {
-
-      const asyncFetch = require('../../src/connectivity/fetch-json-async-await.js').default;
-      asyncFetch.fetchAsJson = jest.fn(() => { return {
-        ok: false,
-        statusText: 'No can do',
-        status: 401
-      }});
-
-      async function doLogin() {
-        return await api.login('baddy', 'badpass');
-      }
-
-      const syncFunction = await helpers.syncify(doLogin);
-      expect(syncFunction).toThrow('No can do');
-
-      let url = asyncFetch.fetchAsJson.mock.calls[0][0];
-      expect(url).toEqual('http://api.rest-user-api.dev/app_acceptance.php/login');
-
-      let requestConfig = asyncFetch.fetchAsJson.mock.calls[0][1];
-      let body = JSON.stringify({ username: 'baddy', password: 'badpass' });
-
-      expect(requestConfig.body).toEqual(body);
-      expect(requestConfig.method).toEqual('POST');
-    });
-
   });
 
 
@@ -66,10 +36,7 @@ describe('Connectivity', () => {
     it('has a happy path', async () => {
 
       const asyncFetch = require('../../src/connectivity/fetch-json-async-await.js').default;
-      asyncFetch.fetchAsJson = jest.fn(() => { return {
-        ok: true,
-        json: () => 'worked'
-      }});
+      asyncFetch.fetchAsJson = jest.fn(() => 'worked');
 
       let result = await api.fetchProfile(123);
       expect(result).toEqual('worked');
@@ -81,35 +48,7 @@ describe('Connectivity', () => {
       expect(requestConfig.method).toEqual('GET');
     });
 
-
-    it('handles the unhappy path', async () => {
-
-      const asyncFetch = require('../../src/connectivity/fetch-json-async-await.js').default;
-
-      asyncFetch.fetchAsJson = jest.fn(() => { return {
-        ok: false,
-        statusText: 'Blew up',
-        status: 400
-      }});
-
-      async function doFetchProfile() {
-        return await api.fetchProfile(123);
-      }
-
-      const syncFunction = await helpers.syncify(doFetchProfile);
-
-      expect(syncFunction).toThrow('Blew up');
-
-      let url = asyncFetch.fetchAsJson.mock.calls[0][0];
-      expect(url).toEqual('http://api.rest-user-api.dev/app_acceptance.php/profile/123');
-
-      let requestConfig = asyncFetch.fetchAsJson.mock.calls[0][1];
-      expect(requestConfig.method).toEqual('GET');
-    });
-
   });
-
-
 
 
   describe('changePassword', () => {
@@ -117,10 +56,7 @@ describe('Connectivity', () => {
     it('has a happy path', async () => {
 
       const asyncFetch = require('../../src/connectivity/fetch-json-async-await.js').default;
-      asyncFetch.fetchAsJson = jest.fn(() => { return {
-        ok: true,
-        json: () => 'worked'
-      }});
+      asyncFetch.fetchAsJson = jest.fn(() => 'worked');
 
       let result = await api.changePassword(123, 'oldpass', 'newpass', 'newpass');
       expect(result).toEqual('worked');
@@ -132,39 +68,6 @@ describe('Connectivity', () => {
         "plainPassword": {
           "first": 'newpass',
           "second": 'newpass'
-        }
-      });
-
-      expect(url).toEqual('http://api.rest-user-api.dev/app_acceptance.php/password/123/change');
-      expect(requestConfig.body).toEqual(body);
-      expect(requestConfig.method).toEqual('POST');
-    });
-
-
-    it('handles the unhappy path', async () => {
-
-      const asyncFetch = require('../../src/connectivity/fetch-json-async-await.js').default;
-      asyncFetch.fetchAsJson = jest.fn(() => { return {
-        ok: false,
-        statusText: 'Passwords do not match',
-        status: 400
-      }});
-
-      async function doChangePassword() {
-        return await api.changePassword(123, 'oldpass', 'newpass', 'newpass-mistake');
-      }
-
-      const syncFunction = await helpers.syncify(doChangePassword);
-
-      expect(syncFunction).toThrow('Passwords do not match');
-
-      let url = asyncFetch.fetchAsJson.mock.calls[0][0];
-      let requestConfig = asyncFetch.fetchAsJson.mock.calls[0][1];
-      let body = JSON.stringify({
-        "current_password": 'oldpass',
-        "plainPassword": {
-          "first": 'newpass',
-          "second": 'newpass-mistake'
         }
       });
 
--- src/connectivity/fetch-json-async-await.js
+++ src/connectivity/fetch-json-async-await.js
@@ -2,13 +2,7 @@ import HttpApiCallError from '../errors/HttpApiCallError';
 
 async function fetchAsJson(url, requestConfig = {}) {
   const resp = await fetch(url, requestConfig);
-
-  console.log('resp', resp);
-
   const data = await resp.json();
-
-  console.log('data', data);
-
 
   const isSuccess = resp.status >= 200 && resp.status < 300;
 
